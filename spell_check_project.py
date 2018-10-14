72fed47eee6e9eafbb17367d7cff52d42c05c1d1
amirkashi <amka6009@colorado.edu>
2018-10-14 12:05:17 -0600

    unigram done

--- spell_check_project.py
+++ spell_check_project.py
@@ -105,12 +105,107 @@ def look_for_word_in_list_massage(word):
         print (word + ": was not found in dictionary so it spelled incorrectly ***")
         look_for_word_in_list(False) 
 
+### -- unigram 
+def find_unigram(word, def_med):
+
+    table = PrettyTable(['Suggested Word', 'Minimum Edit distance', "Probability"])
+
+    corrct_word_med_1 = []
+    corrct_word_med_2 = []
+    corrct_word_med_3 = []
+    for correct_words in word_unigram_prob:
+        temp = []
+        med = minimum_edit_distance(correct_words, word)
+        if 0< med <= def_med:
+            temp.append(correct_words)
+            temp.append(int(med))
+            temp.append(word_unigram_prob[correct_words])
+            if med == 1:
+                corrct_word_med_1.append(temp)
+            elif med == 2:
+                corrct_word_med_2.append(temp)
+            else:
+                corrct_word_med_3.append(temp)
+
+    corrct_word_table_sorted = sorted(corrct_word_med_1,key=lambda l:l[2], reverse=True)
+    corrct_word_med_2_sorted = sorted(corrct_word_med_2,key=lambda l:l[2], reverse=True)
+    for lst in corrct_word_med_2_sorted:
+        corrct_word_table_sorted.append(lst)
+
+    corrct_word_med_3_sorted = sorted(corrct_word_med_3,key=lambda l:l[2], reverse=True)
+    for lst in corrct_word_med_3_sorted:
+        corrct_word_table_sorted.append(lst)
+
+    row = 0
+    for items in corrct_word_table_sorted:
+        if row < row_number_print:
+            table.add_row(items)
+            row+=1
+
+    table.align['Suggested Word'] = "l"
+    table.align['Probability'] = "l"
+
+
+    if len(corrct_word_table_sorted) == 0:
+        def_med +=1
+        find_unigram(word, def_med)
+    elif  6 <= def_med:
+        print (" ")
+        print ("There is not a suggested word for" + word + "in a  reasonable edit distance.")
+        print ("Code does not look for words with 6 or more edit distance")
+    else:
+        print (" ")
+        print ("Sugested words for " + word + " using unigram model are:")
+        print (table)
+
+
+def find_wrong_words(given_words):
+    wrong_words = []
+    for wrd in given_words:
+        look_for_word_in_list_massage(wrd)
+        if look_for_word_in_list(wrd) == False:
+            wrong_words.append(wrd)    
+    return wrong_words
+
+def spell_check(input_sentence, row_number_print, default_med, word_unigram_prob):
+    inp = input_sentence.strip()
+    given_words = word_tokenize(inp)
+
+    check_fist_word_capital(input_sentence)
+    check_sentence_end_punctuation(input_sentence)
+    print ("")
+    
+    wrong_words = find_wrong_words(given_words)
+
+    if not wrong_words:
+        print ("All word of the entered sentence are correct.")
+    else:
+        print ("Fowllowing word or words spelled incorectly:")
+        for word in wrong_words:
+            print (word)
+
+        for word in wrong_words:
+            find_unigram(word, default_med)
+            
+
+    
+
 
 if __name__ == '__main__':
+    print ("Please wait ")
     word_list, tags = read_file('wsj00-18_ss.tag')
     word_unigram_prob = calculate_unigram_probability(word_list)
     sentences = make_sentences(word_list)
     bigram_probs =  calculate_bigram_probability(sentences)
     trigram = find_trigrams(sentences)
+    row_number_print = 10 # number of corect word code suggest to user
+    default_med = 2
+    #input_sentence = raw_input("Please Enter a Aentece: ")
+    input_sentence = "this is a bok"
+    
+    spell_check(input_sentence, row_number_print, default_med, word_unigram_prob)
+    
+    
+    
     
     
